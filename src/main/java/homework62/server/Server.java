183482a6f997ceae6d1e323da5594ef9197f94bd
embreev <e.m.breev@gmail.com>
2019-02-20 02:36:16 +0300

    hw6 added methods

---
+++ src/main/java/homework62/server/Server.java
@@ -0,0 +1,63 @@
+package homework62.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.Vector;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Server {
+    private static final Logger LOGGER = LogManager.getLogger(ClientHandler.class);
+
+    private Vector<ClientHandler> clients;
+
+    public Server() throws SQLException {
+        clients = new Vector<>();
+        ServerSocket server = null;
+        Socket socket = null;
+        try {
+            AuthService.connect();
+            server = new ServerSocket(8189);
+            System.out.println("Сервер запущен. Ожидаем клиентов...");
+            LOGGER.info("Сервер запущен. Ожидаем клиентов...");
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился");
+                new ClientHandler(this, socket);
+               // clients.add(new ClientHandler(this, socket));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            AuthService.disconnect();
+        }
+    }
+
+    public void subscribe(ClientHandler client) {
+        clients.add(client);
+    }
+
+    public void unsubscribe(ClientHandler client) {
+        clients.remove(client);
+    }
+
+
+    public void broadcastMsg(String msg) {
+        for (ClientHandler o : clients) {
+            o.sendMsg(msg);
+        }
+    }
+}
