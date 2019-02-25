caa61f217b1d8b48ada1941e1347b09f6ef15cb0
embreev <e.m.breev@gmail.com>
2019-02-26 00:31:01 +0300

    Option for any range

--- education/src/main/java/homework8/test/App.java
+++ education/src/main/java/homework8/test/App.java
@@ -1,7 +1,6 @@
 package homework8.test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class App {
     static String inputData = "5 1 5 2 2 4 1 4 5 1 5 3 2 4 4 4 5 1 3 4 2 2 1 2 4 4 4 5 4 3 5 4 4 5 5 1 4 1 5 3 1 4 5 3 3 4 2 2 4 4 5 5 1 1 1 4 5 5 \n" +
@@ -11,18 +10,19 @@ public class App {
             " 2 4 5 5 4 2 2 1 5 1 3 4 2 4 2 2 4 3 5 2 2 4 4 4 5 5 2 5 5 2 5 1 1 5 5 4 1 2 4 1 2 2 5 4 5 1 5 4 4 5 5 5 3 3 4 3 3 5\n" +
             " 3 2 2 2 2 2 1 2 5 2 3 4 3 5 5 2 4 5 3 4 3 1 3 2 1 1 5 4 4 2 3 1 3 4 2 4 1 3 5 1 5 3 5 2 3 4 4 1 3 1 5 5 1 2 2 1 3 1 \n" +
             " 5 1 2 2 1 5 1 3 3 2 1 3 2 5 1 1 2 3 5 5 4 3 1 3 3 1 5 4 2 3 4";
-
-    static char [] chars = inputData.replaceAll("\\s+", "").toCharArray();
-    static Map<Character, Integer> hm = new HashMap<>();
+    
+    static String clearInputData = inputData.replaceAll("\\s+", " ");
+    static Map<String, Integer> counter = new HashMap<>();
+    static List<String> values = new ArrayList<>(Arrays.asList(clearInputData.split(" ")));
 
     public static void main(String[] args) {
-        for (char c: chars) {
-            if (!hm.containsKey(c)) {
-                hm.put(c, 1);
+        for (String value: values) {
+            if (!counter.containsKey(value)) {
+                counter.put(value, 1);
             } else {
-                hm.put(c, hm.get(c) + 1);
+                counter.put(value, counter.get(value) + 1);
             }
         }
-        System.out.println(hm);
+        System.out.println(counter);
     }
 }
